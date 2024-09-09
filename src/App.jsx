2c42b8ba0403d29a89709ff9148b6ac094086ec7
viisualworks <dshege4@gmail.com>
2024-09-09 14:12:29 +0300

    complete designing the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 function App() {
-  return (
-    <div className='bg-dark-blue h-screen'>
-     <h1 className="font-mono text-stone-600">Generator</h1>
+return (
+<div className='font-manrope bg-dark-blue h-screen grid place-items-center'>
+  <div className="bg-dark-grayish-blue lg:w-2/5 w-4/5 p-10 pb-16 text-center rounded-2xl relative">
+    <small className="text-neon-green tracking-[.3em] font-medium">ADVICE #117</small>
+    <h1 className="text-light-cyan text-2xl font-bold py-4 pb-8">"If you are working on something that you really care
+      about, you
+      don't have to be pushed"</h1>
+    <img className="mx-auto my-0" src="/src/assets/images/pattern-divider-desktop.svg" alt="" />
+    <div className="absolute bg-neon-green p-3 rounded-full -bottom-5 left-2/4 -translate-x-2/4 ">
+      <img className="hover:animate-spin"
+        src="/src/assets/images/icon-dice.svg" />
     </div>
-  )
+  </div>
+</div>
+)
 }
 
 export default App
